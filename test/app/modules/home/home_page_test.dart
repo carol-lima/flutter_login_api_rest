7f520577007bf152c876b36e37fa7bbca100434d
Carol Lima <carol.lima@guep.com.br>
2020-03-16 17:46:56 -0300

    Initial commit

---
+++ test/app/modules/home/home_page_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_modular/flutter_modular_test.dart';
+
+import 'package:flutter_login_api_rest/app/modules/home/home_page.dart';
+
+main() {
+  testWidgets('HomePage has title', (WidgetTester tester) async {
+    //  await tester.pumpWidget(buildTestableWidget(HomePage(title: 'Home')));
+    //  final titleFinder = find.text('Home');
+    //  expect(titleFinder, findsOneWidget);
+  });
+}
