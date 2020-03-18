347ce128698c1c86d9b39ea463767a5c2553332d
Carol Lima <carol.lima@guep.com.br>
2020-03-18 16:10:42 -0300

    Login form

--- lib/app/login/login_page.dart
+++ lib/app/login/login_page.dart
@@ -14,27 +14,90 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Container (
-              height: 400,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/images/background.png'),
-                  fit: BoxFit.fill
-                )
-              ),
-              child: FlareActor(
-                "assets/images/minion.flr",
-                // from animation editor of 2Dimension list
-                // Dance, Stand, Jump, Wave
-                animation: "Wave",
-              ),
-            )
-          ],
-        ),
+      body: new Stack(
+        children: [
+          Positioned.fill(
+            child: FlareActor("assets/images/leaderboard.flr",
+            fit: BoxFit.cover,
+            animation: "success",
+            )),
+          Padding(
+            padding: EdgeInsets.all(30.0),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  height: 120,
+                  child: Center(
+                    child: Text("Login", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 40)),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color.fromRGBO(143, 148, 251, .2),
+                        blurRadius: 20.0,
+                        offset: Offset(0, 10)
+                      )
+                    ]
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                          border: Border(bottom: BorderSide(color: Colors.grey[100]))
+                        ),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Email",
+                            hintStyle: TextStyle(color: Colors.grey[400])
+                          )
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(8.0),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Password",
+                            hintStyle: TextStyle(color: Colors.grey[400])
+                          )
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(height: 30,),
+                Container(
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Colors.red[600]
+                  ),
+                  child: Center(
+                    child: Text("Login", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
+
+        /*child: FlareActor(
+                "assets/images/leaderboard.flr",
+                // from animation editor of 2Dimension list
+                // idle, fail, success, after_sucess
+                animation: "success",
+                fit: BoxFit.fill
+        ),*/
+        
+      
     );
   }
 }
